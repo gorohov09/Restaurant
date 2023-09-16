444a33a7cc6549fd07fed703eac2602ec0b46ee9
AndrewGorohov <andrej.gorokhoff2017@yandex.ru>
2023-09-16 23:26:58 +0300

    Работа с корзиной в основном веб-приложении

---
+++ Frontend/Restaurant.Web/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Web.Models;
+using Restaurant.Web.Services.IServices;
+using System.Diagnostics;
+
+namespace Restaurant.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ICartService _cartService;
+
+        public CartController(ILogger<HomeController> logger, ICartService cartService)
+        {
+            _logger = logger;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.RemoveFromCartAsync<ResponseDto>(cartDetailsId, accessToken);
+
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.GetCartByUserIdAsnyc<ResponseDto>(userId, accessToken);
+
+            CartDto cartDto = new();
+            if (response != null && response.IsSuccess)
+            {
+                cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
+            }
+
+            if (cartDto.CartHeader != null)
+            {
+                foreach (var detail in cartDto.CartDetails)
+                {
+                    cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+                }
+            }
+
+            return cartDto;
+        }
+    }
+}
